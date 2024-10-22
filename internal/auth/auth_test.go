d021dcbba4182bba20a55a9dcc86f32de079ee7a
Manu-CAI <134920146+Manu-CAI@users.noreply.github.com>
2024-10-22 13:12:28 +0530

    Updated error message in main.go (#2)
    * Updated error message in main.go
    
    * CI github actions added
    
    * Updated CI
    
    * Added auth_test.go; Modified CI
    
    * Added cover to go test command
    
    * Added badge for readme.md
    
    ---------
    
    Co-authored-by: Manu-CAI <manu@Manus-MacBook-Air.local>

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,66 @@
+package auth
+
+import (
+        "errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name        string
+		headers     http.Header
+		expectedKey string
+		expectedErr error
+	}{
+		{
+			name:        "No Authorization Header",
+			headers:     http.Header{},
+			expectedKey: "",
+			expectedErr: ErrNoAuthHeaderIncluded,
+		},
+		{
+			name: "Malformed Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey"},
+			},
+			expectedKey: "",
+			expectedErr: errors.New("malformed authorization header"),
+		},
+		{
+			name: "Correct Authorization Header",
+			headers: http.Header{
+				"Authorization": []string{"ApiKey my-secret-api-key"},
+			},
+			expectedKey: "my-secret-api-key",
+			expectedErr: nil,
+		},
+		{
+			name: "Incorrect Authorization Scheme",
+			headers: http.Header{
+				"Authorization": []string{"Bearer some-token"},
+			},
+			expectedKey: "",
+			expectedErr: errors.New("malformed authorization header"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiKey, err := GetAPIKey(tt.headers)
+
+			if apiKey != tt.expectedKey {
+				t.Errorf("expected API key %v, got %v", tt.expectedKey, apiKey)
+			}
+
+			if err != nil && tt.expectedErr != nil {
+				if err.Error() != tt.expectedErr.Error() {
+					t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+				}
+			} else if err != tt.expectedErr {
+				t.Errorf("expected error %v, got %v", tt.expectedErr, err)
+			}
+		})
+	}
+}
+
